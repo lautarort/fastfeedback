5383bbbb349590090beb88cf0e41d967b6878b95
unknown <lautarorangilt@gmail.com>
2022-03-28 16:55:49 -0300

    absolute imports

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,39 @@
-import { ThemeProvider, CSSReset } from '@chakra-ui/core'
+import { ThemeProvider, CSSReset, useColorMode } from '@chakra-ui/core'
 import { AuthProvider } from '../lib/auth'
 import theme from '../styles/theme'
 import { ChakraProvider } from '@chakra-ui/react'
+import { Global, css } from '@emotion/react'
+
+const GlobalStyle = ({ children }) => {
+
+  return (
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            min-width: 380px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+          `}
+      />
+      {children}
+    </>
+  )
+}
+
 
 const App = ({ Component, pageProps }) => {
   return (
     <ThemeProvider theme={theme}>
       <ChakraProvider>
         <AuthProvider>
-          <CSSReset />
+          <GlobalStyle />
           <Component {...pageProps} />
         </AuthProvider>
       </ChakraProvider>
