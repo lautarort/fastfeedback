34752bd2d1c5274b23cb0e489fae7fb727ffc6c1
unknown <lautarorangilt@gmail.com>
2022-03-30 19:58:33 -0300

    createsite

--- components/AddSiteModal.js
+++ components/AddSiteModal.js
@@ -1,5 +1,5 @@
-import { useRef } from 'react'
 import { useForm } from 'react-hook-form';
+import { mutate } from 'swr';
 import {
     Modal,
     ModalOverlay,
@@ -7,19 +7,54 @@ import {
     ModalHeader,
     ModalFooter,
     ModalBody,
+    ModalCloseButton,
     FormControl,
     FormLabel,
-    ModalCloseButton,
     Button,
     Input,
+    useToast,
     useDisclosure
+} from '@chakra-ui/react';
 
-} from '@chakra-ui/react'
+import { createSite } from '../lib/db';
+// import { useAuth } from '../lib/auth';
 
-const AddSiteModal = () => {
+const AddSiteModal = ({ children }) => {
+    // const toast = useToast();
+    // const auth = useAuth();
     const { isOpen, onOpen, onClose } = useDisclosure();
+    const { handleSubmit, register } = useForm();
 
-    const initialRef = useRef();
+    // const onCreateSite = ({ name, url }) => {
+    //     const newSite = {
+    //         authorId: auth.user.uid,
+    //         createdAt: new Date().toISOString(),
+    //         name,
+    //         url,
+    //         settings: {
+    //             icons: true,
+    //             timestamp: true,
+    //             ratings: false
+    //         }
+    //     };
+
+    //     const { id } = createSite(newSite);
+    //     toast({
+    //         title: 'Success!',
+    //         description: "We've added your site.",
+    //         status: 'success',
+    //         duration: 5000,
+    //         isClosable: true
+    //     });
+    //     mutate(
+    //         ['/api/sites', auth.user.token],
+    //         async (data) => ({
+    //             sites: [{ id, ...newSite }, ...data.sites]
+    //         }),
+    //         false
+    //     );
+    //     onClose();
+    // };
 
     return (
         <>
@@ -33,39 +68,51 @@ const AddSiteModal = () => {
                     transform: 'scale(0.95)'
                 }}
                 onClick={onOpen}> + Add Site</Button>
-
-
-            <Modal
-                initialFocusRef={initialRef}
-                isOpen={isOpen}
-                onClose={onClose}
-            >
+            <Modal isOpen={isOpen} onClose={onClose}>
                 <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>Add Site</ModalHeader>
+                <ModalContent as="form" onSubmit={handleSubmit(createSite)}>
+                    <ModalHeader fontWeight="bold">Add Site</ModalHeader>
                     <ModalCloseButton />
                     <ModalBody pb={6}>
                         <FormControl>
                             <FormLabel>Name</FormLabel>
-                            <Input ref={initialRef} placeholder='My site name' />
+                            <Input
+                                id="site-input"
+                                placeholder="My site"
+                                name="name"
+                                {...register('name', { required: true })}
+                            />
                         </FormControl>
 
                         <FormControl mt={4}>
                             <FormLabel>Link</FormLabel>
-                            <Input placeholder='https://website.com' />
+                            <Input
+                                id="link-input"
+                                placeholder="https://website.com"
+                                name="url"
+                                {...register('link', { required: true })}
+                            />
                         </FormControl>
                     </ModalBody>
 
                     <ModalFooter>
-                        <Button colorScheme='teal' >
+                        <Button onClick={onClose} mr={3} fontWeight="medium">
+                            Cancel
+                        </Button>
+                        <Button
+                            id="create-site-button"
+                            backgroundColor="#99FFFE"
+                            color="#194D4C"
+                            fontWeight="medium"
+                            type="submit"
+                        >
                             Create
                         </Button>
-                        <Button onClick={onClose} mr={3}>Cancel</Button>
                     </ModalFooter>
                 </ModalContent>
             </Modal>
         </>
-    )
-}
+    );
+};
 
 export default AddSiteModal;
